61d217a895d67b5db0f115862975f7a1fa9a7b84
khvnh3inh <nt5eyes@gmail.com>
2026-09-10 08:09:24 +0700

    giao dien

---
+++ Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI;
+
+namespace FoodieProject
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Trang chính – mọi logic xử lý ở phía client (JavaScript + Web API)
+        }
+    }
+}
